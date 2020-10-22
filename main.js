14f05303ac62c381d6b2e0ac1ec00b07b8bb7f1e
noisya55616 <noisya55616@gmail.com>
2020-10-22 19:26:41 -0400

    Use filename of opened file when saving

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+var fileName = "";
+
+/* Renders markdown from the textarea into HTML to display in the div. */
+
 function render() {
     var converter = new showdown.Converter({simplifiedAutoLink: 'true'}, {ghMentions: 'true'}, {ghMentionsLink: 'true'}, {ghCompatibleHeaderId: 'true'}, {strikethrough: 'true'}, {tasklists: 'true'}, {smoothLivePreview: 'true'}, {openLinksInNewWindow: 'true'}, {emoji: 'true'}, {underline: 'true'}, {metadata: 'true'});
     converter.setFlavor('github');
@@ -7,11 +11,15 @@ function render() {
     target.innerHTML = html;
 }
 
+/* Just saves the text from the textarea into a file on disk. */
+
 function saveEntryBoxToFile() {
     var text = document.getElementById("entryBox").value;
     var blob = new Blob([text], { type: "text/plain;charset=utf-8" });
-    saveAs(blob, "save.md");
+    saveAs(blob, fileName);
 }
+
+/* Exports the HTML conversion of the markdown to a file on disk. */
 
 function exportToHTML() {
     var text = document.getElementById("entryBox").value;
@@ -28,6 +36,8 @@ function readFile(file) {
       fr.readAsText(file);
   })}
 
+/* Each of these are made to give focus back to the textarea once they're done. */
+
 function saveButtonClicked() {
     saveEntryBoxToFile();
     document.getElementsByTagName('textarea')[0].focus();
@@ -37,9 +47,33 @@ function exportButtonClicked() {
     exportToHTML();
     document.getElementsByTagName('textarea')[0].focus();
 }
-  
+
+/* This is for reading in a file off the disk and putting it into the textarea. */
+
 async function read(input) {
     var text = await readFile(input.files[0]);
+    var fileInfo = document.getElementById("loadButton");
+    fileName = fileInfo.files.item(0).name;
     document.getElementById("loadButton").value = "";
-    document.getElementById("entryBox").value = text; 
+    document.getElementById("entryBox").value = text;
+    document.getElementById("boxTitle").innerHTML = fileName;
+    render();
 }
+
+/* This enables use to actually use the tab key in the textarea, for easier formatting of the source file. */
+
+document.getElementById('entryBox').addEventListener('keydown', function(e) {
+    if (e.key == 'Tab') {
+      e.preventDefault();
+      var start = this.selectionStart;
+      var end = this.selectionEnd;
+  
+      // set textarea value to: text before caret + tab + text after caret
+      this.value = this.value.substring(0, start) +
+        "\t" + this.value.substring(end);
+  
+      // put caret at right position again
+      this.selectionStart =
+        this.selectionEnd = start + 1;
+    }
+  });
